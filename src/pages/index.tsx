0163aedecc73aec5a84a426d3b81991d1d77c436
gyurilee <jae04099@gmail.com>
2021-12-23 09:10:56 +0900

    :sparkles: make scroll select section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,39 @@
 import type { NextPage } from 'next'
+import { useState } from 'react';
 import styled from 'styled-components'
 const Home: NextPage = () => {
   const END_POINT = "https://wine-likelion-com-gjkmogijw-jae04099.vercel.app/";
+  const [isWineClicked, setWineClick] = useState(false);
+  const [isBeerClicked, setBeerClick] = useState(false);
+
+  const handleWineClick = () => {
+    setWineClick(!isWineClicked)
+  }
+  const handleBeerClick = () => {
+    setBeerClick(!isBeerClicked)
+  }
   return (
     <Container>
-      <a href={`${END_POINT}wines/Port`}>Wine🍷</a>
-      <a href={`${END_POINT}beers/ale`}>Beer🍺</a>
+      <InnerContainer>
+      <WineContainer>
+      <a onClick={handleWineClick}>Wine🍷</a>
+      {isWineClicked ? (<ul>
+        <li><a href={`${END_POINT}wines/Dessert`}>Dessert</a></li>
+        <li><a href={`${END_POINT}wines/Port`}>Port</a></li>
+        <li><a href={`${END_POINT}wines/Reds`}>Reds</a></li>
+        <li><a href={`${END_POINT}wines/Rose`}>Rose</a></li>
+        <li><a href={`${END_POINT}wines/Sparkling`}>Sparkling</a></li>
+        <li><a href={`${END_POINT}wines/Whites`}>Whites</a></li>
+      </ul>) : ''}
+      </WineContainer>
+      <BeerContainer>
+      <a onClick={handleBeerClick}>Beer🍺</a>
+      {isBeerClicked ? (<ul>
+        <li><a href={`${END_POINT}wines/ale`}>Ale</a></li>
+        <li><a href={`${END_POINT}wines/stouts`}>Stouts</a></li>
+      </ul>) : ''}
+      </BeerContainer>
+      </InnerContainer>
     </Container>
   )
 }
@@ -22,7 +50,7 @@ const Container = styled.div`
   a {
     color: #fff;
     font-size: 32px;
-    padding: 15px;
+    padding: 10px;
     background: #4800d9;
     border-radius: 20px;
     margin-right: 30px;
@@ -31,4 +59,31 @@ const Container = styled.div`
   a:hover {
     transform: translateY(-20px);
   }
+  ul {
+    padding: 0;
+    
+  }
+  li {
+    list-style: none;
+    margin-top: 30px;
+  }
+`
+
+const WineContainer = styled.div`
+li > a {
+   font-size: 20px;
+   background: transparent;
+   border: 3px solid #4800d9;
+ }
+`
+const BeerContainer = styled.div`
+li > a {
+  font-size: 20px;
+   background: transparent;
+   border: 3px solid #4800d9;
+ }
+`
+
+const InnerContainer = styled.div`
+  display: flex;
 `
